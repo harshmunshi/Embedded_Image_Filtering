58ae9b5b5a42687917029b42f6056e6a1566a6ec
Harsh Hemangkumar Munshi <harsh.hemangkumar.munshi@iais.fraunhofer.de>
2020-07-07 23:47:13 +0200

    planned exec

--- experiments/imagejpg.cpp
+++ experiments/imagejpg.cpp
@@ -9,7 +9,11 @@ Mat Image::open(string fname)
 {
     ifstream file{fname.c_str(), ios::in | ios::binary};
     // define a matrix element
-
+    /*
+    Step 1: Allot the memory size of header and seek forward
+    Step 2: Check if is it a legit jpeg file
+    Step 3: Read the data in sequence and allot to the pixel class.
+    */
 }
 
 int main(int argc, char** argv){
